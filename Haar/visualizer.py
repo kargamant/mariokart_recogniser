c7b78f8d1250ab633f2ab04e1e461c2e766e1f5f
kargamant <firexholms@gmail.com>
2025-01-23 17:56:31 +0300

    More broad options to running program in terminal

--- Haar/visualizer.py
+++ Haar/visualizer.py
@@ -1,6 +1,6 @@
 import cv2
 import os
-
+from time import sleep
 
 class Visualizer:
 
@@ -12,6 +12,7 @@ class Visualizer:
                 cv2.imwrite(os.path.join(save_dir, f'{i}.jpg'), img)
                 i += 1
             else:
+                sleep(1)
                 cv2.imshow('image', img)
                 cv2.waitKey(0)
                 cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -2,30 +2,52 @@ import cv2
 from Haar import DataPreparator, Trainer, Detector, Visualizer
 import os
 from environments import *
-import sys
+import argparse
 
 
 if __name__ == '__main__':
     # todo: choice of Haar vs YOLO with arguments from cmd
     # todo: also make an option to choose between boo and coins recognition
 
-    options = []
-    for i in range(1, len(sys.argv)):
-        options.append(sys.argv[i])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-prep', '--preparator', action='store_true')
+    parser.add_argument('-train', '--trainer', action='store_true')
+    parser.add_argument('-all', '--all', action='store_true')
+    parser.add_argument('-one_file', '--one_file')
+    parser.add_argument('-w', '--width', type=int, default=70)
+    parser.add_argument('-he', '--height', type=int, default=70)
+    parser.add_argument('-pos', '--positives', type=int, default=70)
+    parser.add_argument('-neg', '--negatives', type=int, default=50)
+    parser.add_argument('-st', '--stages', type=int, default=5)
+    parser.add_argument('-mnb', '--minNeighbours', type=int, default=20)
+    parser.add_argument('-test', '--test', action='store_true')
+    parser.add_argument('-nimg', '--nimages', type=int, default=30)
+    parser.add_argument('-write', '--write_res', action='store_true')
+    parser.add_argument('-res', '--results_dir', type=str, default='results')
+
+
+    args = parser.parse_args()
 
     # full pipeline
-    if '-prep' in options or 'all' in options:
+    if args.preparator or args.all:
         dp = DataPreparator(os.path.join(CURRENT_DIR, BOO_DIR), os.path.join(CURRENT_DIR, GP_DIR))
         dp.prepare_labels()
 
-    if '-train' in options or 'all' in options:
-        Trainer.create_vec_file(GOOD_FILE_DIR, VEC_FILE_DIR, 70, 70)
-        Trainer.train_cascade(MODEL_DIR, VEC_FILE_DIR, BAD_FILE_DIR, 70, 50, 5, 70, 70)
+    if args.trainer or args.all:
+        # todo automatically clean data dir
+        Trainer.create_vec_file(GOOD_FILE_DIR, VEC_FILE_DIR, args.width, args.height)
+        Trainer.train_cascade(MODEL_DIR, VEC_FILE_DIR, BAD_FILE_DIR, args.positives, args.negatives, args.stages, args.width, args.height)
 
     detector = Detector(os.path.join(CURRENT_DIR, MODEL_DIR, 'cascade.xml'))
 
-    test_results = []
-    for img in os.listdir(os.path.join(CURRENT_DIR, BOO_DIR, 'test', 'images'))[:20:]:
-          test_results.append(detector.detect(os.path.join(CURRENT_DIR, BOO_DIR, 'test', 'images', str(img)), 20, (70, 70)))
-          print(img)
-    Visualizer.viz(test_results, write=0, save_dir='results')
+    if args.one_file:
+        result = detector.detect(args.one_file, args.minNeighbours, (args.width, args.height))
+        Visualizer.viz([result])
+    elif args.test:
+        # testing
+        test_results = []
+        for img in os.listdir(os.path.join(CURRENT_DIR, BOO_DIR, 'valid', 'images'))[:args.nimages:]:
+              test_results.append(detector.detect(os.path.join(CURRENT_DIR, BOO_DIR, 'valid', 'images', str(img)), args.minNeighbours, (args.width, args.height)))
+              print(img)
+        Visualizer.viz(test_results, write=args.write_res, save_dir=args.results_dir)
+    print('Bye!')
